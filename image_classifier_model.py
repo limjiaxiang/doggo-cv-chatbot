0b816ed54ad60051d1ce03c6a3f59bf2b899c2e5
limjiaxiang <32549573+limjiaxiang@users.noreply.github.com>
2018-07-03 09:03:44 +0200

    Modified transfer learning function, save weights instead of the entire model for ModelCheckpoint

--- image_classifier_model.py
+++ image_classifier_model.py
@@ -3,15 +3,10 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.externals import joblib
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 from keras import Sequential
 from keras.models import load_model
-from keras.applications import InceptionV3, ResNet50, MobileNet, DenseNet121
-# from keras.applications.inception_v3 import preprocess_input
-# from keras.applications.resnet50 import preprocess_input
-# from keras.applications.mobilenet import preprocess_input
-# from keras.applications.densenet import preprocess_input
+from keras.applications import InceptionV3
+from keras.applications.inception_v3 import preprocess_input
 from keras.preprocessing.image import ImageDataGenerator
 from keras import optimizers, metrics, losses
 from keras.callbacks import ModelCheckpoint, EarlyStopping
@@ -64,11 +59,11 @@ def model_builder(model_input, model_output):
     return model
 
 
-def transfer_learning_model(model_input, model_output):
+def transfer_learning_model(image_shape, num_classes):
     model = Sequential()
-    model.add(InceptionV3(include_top=False, weights='imagenet', input_shape=tuple(model_input.shape[1:]),
+    model.add(InceptionV3(include_top=False, weights='imagenet', input_shape=image_shape,
                           pooling='avg'))
-    model.add(Dense(model_output.shape[1], activation='softmax'))
+    model.add(Dense(num_classes, activation='softmax'))
     model.layers[0].trainable = False
     op = optimizers.RMSprop()
     model.compile(optimizer=op, metrics=['accuracy'], loss='categorical_crossentropy')
@@ -78,59 +73,46 @@ def transfer_learning_model(model_input, model_output):
 def model_train(model, model_name, training_input=None, training_output=None, batch_size=64, epochs=100000, verbose=1,
                 test_input=None, test_output=None, generator=None, use_multiprocessing=False, max_workers=1,
                 max_queue_size=5):
-    save_checkpoint = ModelCheckpoint(model_name, save_best_only=True, verbose=1)
+    save_checkpoint = ModelCheckpoint(model_name, save_best_only=True, verbose=1, save_weights_only=True)
     early_stop = EarlyStopping(min_delta=0.01, patience=200, verbose=1, mode='min')
     if test_input is None:
         validation_data = None
+        save_checkpoint.monitor = 'loss'
+        early_stop.monitor = 'loss'
     else:
         validation_data = (test_input, test_output)
+    callbacks = [save_checkpoint, early_stop]
     if generator:
-        model.fit_generator(generator=generator, steps_per_epoch=(generator.x.shape[0]//batch_size), epochs=epochs,
-                            verbose=verbose, callbacks=[save_checkpoint, early_stop], validation_data=validation_data,
-                            use_multiprocessing=use_multiprocessing, workers=max_workers,  max_queue_size=max_queue_size)
+        model.fit_generator(generator=generator, steps_per_epoch=(generator.n//batch_size), epochs=epochs,
+                            verbose=verbose, callbacks=callbacks, validation_data=validation_data,
+                            use_multiprocessing=use_multiprocessing, workers=max_workers,
+                            max_queue_size=max_queue_size)
     elif training_input and training_output:
         model.fit(training_input, training_output, batch_size=batch_size, epochs=epochs, verbose=verbose, shuffle=True,
-                  callbacks=[save_checkpoint, early_stop], validation_data=validation_data)
+                  callbacks=callbacks, validation_data=validation_data)
 
 
 if __name__ == '__main__':
 
-    name = 'doggo_classifier.h5'
-
     # processor = ImageProcessor('images', '../image_scrapper/breeds.csv', save_encoding_fname='y_encoding.pkl')
     # x, y = processor.load_images(pad=True)
-    #
-    # os.chdir('..')
-    #
-    # joblib.dump(x, 'x_dataset_normalise.pkl', compress=9)
-    # joblib.dump(y, 'y.pkl', compress=9)
-
-    x = joblib.load('x_dataset_normalise_stretch.pkl')
-    y = joblib.load('y.pkl')
-
     # train_x, test_x, train_y, test_y = train_test_split(x, y, test_size=0.15, random_state=15, stratify=np.unique(y))
-    #
-    # datagen = ImageDataGenerator(rotation_range=45, horizontal_flip=True, data_format='channels_last', zoom_range=0.2,
-    #                              width_shift_range=0.2, height_shift_range=0.2, fill_mode='nearest', shear_range=0.2)
 
-    # bth_size = 64
-    # doggo_model = model_builder(train_x, train_y)
-    # doggo_model = load_model(name)
-    # doggo_model = transfer_learning_model(train_x, train_y)
-    # model_train(doggo_model, name, generator=datagen.flow(train_x, train_y, batch_size=bth_size),
-    #             test_input=test_x, test_output=test_y, max_queue_size=True, max_workers=4)
+    name = 'doggo_classifier.h5'
+    batch_size = 64
+    image_shape = (250, 250, 3)
+    number_of_classes = len(os.listdir('data/train'))
 
-    plt.subplot(1, 2, 1)
-    plt.title(np.argmax(y[np.where(y[5] == 1)[0][0]]))
-    plt.axis('off')
-    plt.imshow(x[5].astype(np.uint8))
+    datagen = ImageDataGenerator(rotation_range=45, horizontal_flip=True, data_format='channels_last', zoom_range=0.2,
+                                 width_shift_range=0.2, height_shift_range=0.2, fill_mode='nearest', shear_range=0.2,
+                                 preprocessing_function=preprocess_input)
 
-    plt.subplot(1, 2, 2)
-    plt.title(np.argmax(y[np.where(y[7] == 1)[0][0]]))
-    plt.axis('off')
-    plt.imshow(x[7].astype(np.uint8))
-    plt.show()
+    train_generator = datagen.flow_from_directory(r'data\train', target_size=image_shape[:-1], batch_size=batch_size,
+                                                  class_mode='categorical')
 
-    print('hello')
+    doggo_model = transfer_learning_model(image_shape=image_shape, num_classes=number_of_classes)
+    doggo_model.load_weights(name)
 
-    # InceptionV3(include_top=False, pooling='avg', )
+    model_train(doggo_model, name, generator=train_generator, max_queue_size=True, max_workers=4)
+
+    print('training_completed')
